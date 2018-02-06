43561ccdaada5fafb756cebfb59499a1fd6be17e
Aaron Evans <aaron.evans@saucelabs.com>
2018-02-05 21:42:52 -0700

    my first take

--- lib/coding_test.rb
+++ lib/coding_test.rb
@@ -1,3 +1,6 @@
+require_relative "./weather_record.rb"
+require_relative "./team_record.rb"
+
 module CodingTest
 
     # Feel free to use, ignore or modify this code as you see fit
@@ -9,12 +12,64 @@ module CodingTest
     file.close
   end
 
+
   def day_min_temp_spread(file_name)
-    fail "Implement this method"
+    # puts "get temp data from " + file_name
+
+    lowest = nil
+
+    load_file file_name do |line|
+      weather = WeatherRecord.new(line)
+
+      # check for invalid weather record
+      if ! weather.parsed?
+        next
+      end
+
+      # check for invalid day
+      if weather.day < 1 or weather.day > 31
+        next
+      end
+
+      if lowest == nil
+        lowest = weather
+      end
+
+      if weather.temp_diff < lowest.temp_diff
+        lowest = weather
+      end
+    end
+
+    puts "Day #{lowest.day} has lowest temperature spread: #{lowest.temp_diff}"
+
+    return lowest.day.to_s
   end
 
+
+
   def team_min_point_spread(file_name)
-    fail "Implement this method"
+    # puts "get point spread from " + file_name
+
+    teams = []
+
+    load_file file_name do |line|
+      team = TeamRecord.new line
+
+      # check for invalid team record
+      if ! team.parsed?
+        puts "invalid team #{team.inspect}"
+        next
+      end
+
+      teams.push team
+      
+    end
+
+    teams.sort! { |a, b|  a.spread <=> b.spread }
+    
+    puts "#{teams.first.name} has lowest point spread: #{teams.first.spread}"
+
+    return teams.first.name
   end
 
 end
---
+++ lib/weather_record.rb
@@ -0,0 +1,36 @@
+class WeatherRecord
+
+  # attr_reader :Dy, :MxT, :MnT, :AvT, :HDDay, :AvDP, :_1HrP, :TPcpn, , :WxType, :PDir, :AvSp, :Dir, :MxS, :SkyC, :MxR, :MnR, :AvSLP
+  attr_reader :day, :max_temp, :min_temp, :avg_temp
+
+  def initialize(line)
+    self.parse(line)
+  end
+
+  def parse(line)
+    @record = line.strip.split(/\s+/)
+    
+    if ! parsed?
+      puts "unable to parse record: #{line}"
+      return false
+    end
+
+    @day= @record[0].to_i    
+    @max_temp= @record[1].to_i
+    @min_temp= @record[2].to_i
+    @avg_temp= @record[3].to_i
+    
+    #TODO: implement parsing of the rest of this with variable field count based on string index
+
+  end
+
+  def parsed?()
+    # don't parse records with unexpected number of fields
+    @record.size >= 4
+  end
+
+  def temp_diff()
+    @max_temp - @min_temp
+  end
+
+end
